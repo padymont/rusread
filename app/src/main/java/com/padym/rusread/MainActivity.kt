1abf6abb6c28dfda960186da7a22a13bcbf94e5c
padymont <polinadymont@yandex.ru>
2024-07-12 14:16:50 +0200

    navigation between two screens

--- app/src/main/java/com/padym/rusread/MainActivity.kt
+++ app/src/main/java/com/padym/rusread/MainActivity.kt
@@ -1,21 +1,32 @@
 package com.padym.rusread
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -25,44 +36,49 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.padym.rusread.ui.theme.RusreadTheme
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RusreadTheme {
                 setContent {
-                    SyllableSelectionScreen()
+                    navController = rememberNavController()
+                    setupNavGraph(navController)
                 }
             }
         }
     }
 }
 
-data class SyllableGroup(val syllables: List<String>)
-
-fun buildRussianSyllableGroups(): List<SyllableGroup> {
-    val consonants = "бвгджзклмнпрстфхцчшщ"
-    val vowels = "аеёиоуыэюя"
-    val special = "йъ"
-
-    return consonants.map { letter ->
-        val syllables = vowels.map { vowel -> letter.toString() + vowel } +
-                (letter.toString() + "ь") +
-                letter.toString()
-        SyllableGroup(syllables)
-    } + SyllableGroup(vowels.map { it.toString() }) +
-            SyllableGroup(special.map { it.toString() })
+@SuppressLint("ComposableNaming")
+@Composable
+fun setupNavGraph(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.SyllableSelection.route
+    ) {
+        composable(Screen.SyllableSelection.route) { SyllableSelectionScreen(navController) }
+        composable(Screen.SyllableGame.route) { backStackEntry ->
+            val chosenSyllables = Screen.SyllableGame.parseChosenSyllables(backStackEntry)
+            SyllableGameScreen(navController, chosenSyllables)
+        }
+    }
 }
 
 @Composable
-fun SyllableSelectionScreen(initialSelectedCount: Int = 0) {
-    var selectedSyllablesCount by remember { mutableIntStateOf(initialSelectedCount) }
-    val syllableGroups = buildRussianSyllableGroups()
+fun SyllableSelectionScreen(navController: NavHostController) {
+    var selectedSyllables by remember { mutableStateOf(emptySet<String>()) }
 
     Column(
         modifier = Modifier
@@ -79,23 +95,105 @@ fun SyllableSelectionScreen(initialSelectedCount: Int = 0) {
         )
 
         LazyColumn(modifier = Modifier.weight(1f)) {
-            items(syllableGroups) { group ->
+            items(buildSyllableGroups()) { group ->
                 SyllableGroupItem(group) { syllable ->
-                    if (syllable.isSelected) {
-                        selectedSyllablesCount++
+                    selectedSyllables = if (syllable.isSelected) {
+                        selectedSyllables + syllable.text
                     } else {
-                        selectedSyllablesCount--
+                        selectedSyllables - syllable.text
                     }
                 }
             }
         }
 
-        if (selectedSyllablesCount >= 3) {
+        if (selectedSyllables.size >= 3) {
             Button(
-                onClick = { /* Handle next button click */ },
+                onClick = {
+                    navController.navigate(
+                        Screen.SyllableGame.passChosenSyllables(selectedSyllables)
+                    )
+                },
                 modifier = Modifier.align(Alignment.CenterHorizontally)
             ) {
                 Text("Next")
+            }
+        }
+    }
+}
+
+data class SyllableGroup(val syllables: List<String>)
+
+fun buildSyllableGroups(): List<SyllableGroup> {
+    val consonants = "бвгджзклмнпрстфхцчшщ"
+    val vowels = "аеёиоуыэюя"
+
+    val consonantSyllableGroupsList = consonants.map { char ->
+        val letter = char.toString()
+        val syllables = vowels.map { letter + it } + (letter + "ь") + letter
+        SyllableGroup(syllables)
+    }
+    val vowelSyllableGroup = SyllableGroup(vowels.map { it.toString() })
+    val specialSyllableGroup = SyllableGroup(listOf("й", "ъ"))
+
+    return consonantSyllableGroupsList + vowelSyllableGroup + specialSyllableGroup
+}
+
+@OptIn(ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun SyllableGameScreen(
+    navController: NavHostController,
+    chosenSyllables: Set<String>,
+) {
+    var correctAnswers by remember { mutableIntStateOf(0) }
+    var newSyllable by remember { mutableStateOf(chosenSyllables.random()) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("") }, // Empty title
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(Icons.Filled.Close, contentDescription = "Close")
+                    }
+                })
+        }
+    ) { paddingValues ->
+        if (correctAnswers < 10 && newSyllable.isNotEmpty()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+                Text(
+                    text = newSyllable,
+                    fontSize = 48.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(top = 32.dp)
+                )
+
+                FlowRow(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                ) {
+                    chosenSyllables.forEach { syllable ->
+                        Button(onClick = {
+                            if (syllable == newSyllable) {
+                                correctAnswers++
+                            }
+                            newSyllable = chosenSyllables.random()
+                        }) {
+                            Text(syllable)
+                        }
+                    }
+                }
+            }
+        } else {
+            // Game finished
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Text("Great job!", fontSize = 32.sp)
             }
         }
     }
@@ -140,8 +238,8 @@ fun SyllableItem(syllable: String, isSelected: Boolean, onToggle: (Boolean) -> U
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SyllableSelectionScreen(initialSelectedCount = 3)
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    SyllableSelectionScreen(initialSelectedCount = 3)
+//}
---
+++ app/src/main/java/com/padym/rusread/Screen.kt
@@ -0,0 +1,25 @@
+package com.padym.rusread
+
+import androidx.navigation.NavBackStackEntry
+
+const val CHOSEN_SYLLABLES_KEY = "chosenSyllables"
+const val STRING_DELIMITER = ", "
+
+sealed class Screen(val route: String) {
+    data object SyllableSelection : Screen("syllable_selection")
+
+    data object SyllableGame : Screen("syllable_game/{$CHOSEN_SYLLABLES_KEY}") {
+
+        fun passChosenSyllables(selectedSyllables: Set<String>): String {
+            val string = selectedSyllables.joinToString(separator = STRING_DELIMITER)
+            return this.route.replace(oldValue = "{$CHOSEN_SYLLABLES_KEY}", newValue = string)
+        }
+
+        fun parseChosenSyllables(backStackEntry: NavBackStackEntry): Set<String> {
+            return backStackEntry.arguments
+                ?.getString(CHOSEN_SYLLABLES_KEY)
+                ?.split(STRING_DELIMITER)?.toSet()
+                ?: emptySet()
+        }
+    }
+}
