528593a013bafce3d4aaff76f7c3bd2589993721
padymont <polinadymont@yandex.ru>
2024-08-04 09:57:37 +0200

    fixed initialisation TextToSpeech

--- app/src/main/java/com/padym/rusread/viewmodels/SyllableGameViewModel.kt
+++ app/src/main/java/com/padym/rusread/viewmodels/SyllableGameViewModel.kt
@@ -15,18 +15,44 @@ const val PROGRESS_OFFSET = 0.3f
 
 class SyllableGameViewModel(application: Application) : AndroidViewModel(application) {
 
+    private val context = application
+    private lateinit var textToSpeech: TextToSpeech
+    init {
+        textToSpeech = TextToSpeech(context) { status ->
+            if (status == TextToSpeech.SUCCESS) {
+                val result = textToSpeech.setLanguage(Locale("ru"))
+                val voice =
+                    textToSpeech.voices?.find { it.name.contains("ru-ru-x-rud-network") }
+                textToSpeech.setVoice(voice)
+                if (result == TextToSpeech.LANG_MISSING_DATA ||
+                    result == TextToSpeech.LANG_NOT_SUPPORTED
+                ) {
+                    Toast.makeText(
+                        context,
+                        "Russian language not supported",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+            } else {
+                Toast.makeText(context, "TTS initialization failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     private val _selectedSyllables = mutableStateOf<Set<String>>(emptySet())
+
     val selectedSyllables: Set<String>
         get() = _selectedSyllables.value
-
     private val _newSyllable = mutableStateOf("")
+
     val newSyllable: String
         get() {
             if (_newSyllable.value.isEmpty()) setNewSyllable()
             return _newSyllable.value
         }
-
     private val _correctAnswers = mutableIntStateOf(0)
+
     val correctAnswers: Int
         get() = _correctAnswers.intValue
 
@@ -57,29 +83,6 @@ class SyllableGameViewModel(application: Application) : AndroidViewModel(applica
 
     private fun setNewSyllable() {
         _newSyllable.value = selectedSyllables.random()
-    }
-
-    private val context = application
-    private val textToSpeech: TextToSpeech by lazy {
-        TextToSpeech(context) { status ->
-            if (status == TextToSpeech.SUCCESS) {
-                val result = textToSpeech.setLanguage(Locale("ru"))
-                val voice = textToSpeech.voices?.find { it.name.contains("ru-ru-x-rud-network") }
-                textToSpeech.setVoice(voice)
-                if (result == TextToSpeech.LANG_MISSING_DATA ||
-                    result == TextToSpeech.LANG_NOT_SUPPORTED
-                ) {
-                    Toast.makeText(
-                        context,
-                        "Russian language not supported",
-                        Toast.LENGTH_SHORT
-                    )
-                        .show()
-                }
-            } else {
-                Toast.makeText(context, "TTS initialization failed", Toast.LENGTH_SHORT).show()
-            }
-        }
     }
 
     fun speakText(text: String) = textToSpeech.speak(
