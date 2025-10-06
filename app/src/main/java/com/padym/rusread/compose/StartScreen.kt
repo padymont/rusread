7bfbf1d22afda20fc40b6c6fc6d4c290eada4812
padymont <61999850+padymont@users.noreply.github.com>
2025-10-06 10:02:18 +0200

    updated hilt viewmodel

--- app/src/main/java/com/padym/rusread/compose/StartScreen.kt
+++ app/src/main/java/com/padym/rusread/compose/StartScreen.kt
@@ -32,7 +32,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.padym.rusread.ui.theme.RusreadTheme
 import com.padym.rusread.viewmodels.StartViewModel
 import com.padym.rusread.viewmodels.SyllablePreview
