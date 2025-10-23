994e22d5baf6bca784b648e000d2e5038dccc742
padymont <61999850+padymont@users.noreply.github.com>
2025-10-23 13:13:30 +0200

    incremented version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.padym.rusread"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
