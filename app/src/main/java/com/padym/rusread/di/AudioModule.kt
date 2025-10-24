f3b4629b636255256f556b95c74c5480b0661ec4
padymont <61999850+padymont@users.noreply.github.com>
2025-10-24 11:33:32 +0200

    fixed merge

--- app/src/main/java/com/padym/rusread/di/AudioModule.kt
+++ app/src/main/java/com/padym/rusread/di/AudioModule.kt
@@ -2,7 +2,6 @@ package com.padym.rusread.di
 
 import android.content.Context
 import com.padym.rusread.SyllableMediaPlayer
-import com.padym.rusread.data.SyllableRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,10 +15,7 @@ object AudioModule {
 
     @Provides
     @Singleton
-    fun provideMediaPlayer(
-        @ApplicationContext context: Context,
-        syllableRepository: SyllableRepository
-    ): SyllableMediaPlayer {
-        return SyllableMediaPlayer(context, syllableRepository)
+    fun provideMediaPlayer(@ApplicationContext context: Context): SyllableMediaPlayer {
+        return SyllableMediaPlayer(context)
     }
 }
